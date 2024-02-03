898f7f3594bd9255d801ed7153977ca26c9101bd
Aos Dabbagh <25783780+aos@users.noreply.github.com>
2024-02-03 18:23:24 -0500

    set a timeout with threads

--- src/bt.rs
+++ src/bt.rs
@@ -1,21 +1,35 @@
 use std::io::{BufRead, BufReader};
 use std::process::{Command, Stdio};
+use std::sync::mpsc;
+use std::thread;
+use std::time::Duration;
 use xshell::Shell;
-
-// bt () {
-// local choice="${1:-1}"
-//
-//  bluetoothctl info "${choice}" | grep 'Connected: yes' &>/dev/null || \
-//    bluetoothctl connect "${choice}"
-//}
 
 // TODO: handle some errors
 const PODS: [&str; 2] = ["E0:EB:40:42:4C:B8", "20:15:82:3C:11:DC"];
+const TIMEOUT_SEC: usize = 60;
 
 pub(crate) fn run(_: &Shell) -> anyhow::Result<()> {
-    let mut pod = "";
+    if let Some(pod) = find_pod(PODS, TIMEOUT_SEC)? {
+        println!("found pod: {}", pod);
 
-    println!("Scanning for... one of {:?}", PODS);
+        println!("Pairing...");
+        wrap_cmd(&["pair", &pod], "Pairing successful")?;
+
+        println!("Connecting...");
+        wrap_cmd(&["connect", &pod], "Connection successful")?;
+    } else {
+        anyhow::bail!(
+            "Failed to find one of {:?} in the last {}",
+            PODS,
+            TIMEOUT_SEC,
+        )
+    }
+    Ok(())
+}
+
+fn find_pod(pods: [&str; 2], timeout: usize) -> anyhow::Result<Option<String>> {
+    let mut pod = None;
     let mut cmd = Command::new("/usr/bin/stdbuf")
         .args(["-o0", "bluetoothctl"])
         .args(["scan", "on"])
@@ -26,23 +40,36 @@ pub(crate) fn run(_: &Shell) -> anyhow::Result<()> {
         .take()
         .ok_or_else(|| anyhow::format_err!("Failed to get stdout"))?;
 
+    // set a timeout
+    let (tx_timeout, rx_timeout) = mpsc::channel();
+    let (tx_found, rx_found) = mpsc::channel();
+    let handle = thread::spawn(move || {
+        for _ in 0..timeout {
+            if let Ok(_) = rx_found.try_recv() {
+                return
+            }
+            thread::sleep(Duration::from_secs(1));
+        }
+        tx_timeout.send(true).unwrap();
+    });
+
+    // search
     let mut scan_reader = BufReader::new(out);
     let mut line = String::new();
     while scan_reader.read_line(&mut line)? > 0 {
-        if let Some(p) = PODS.iter().find(|&&p| line.contains(p)) {
-            pod = p;
+        if let Some(&p) = pods.iter().find(|&&p| line.contains(p)) {
+            pod = Some(p.to_owned());
+            tx_found.send(true)?;
             break;
         }
         line.clear();
+        if let Ok(_) = rx_timeout.try_recv() {
+            break;
+        }
     }
-    println!("found pod: {}", pod);
-
-    println!("Pairing...");
-    wrap_cmd(&["pair", &pod], "Pairing successful")?;
-
-    println!("Connecting...");
-    wrap_cmd(&["connect", &pod], "Connection successful")?;
-    Ok(())
+    cmd.kill()?;
+    handle.join().unwrap();
+    Ok(pod)
 }
 
 fn wrap_cmd(args: &[&str], search: &str) -> anyhow::Result<()> {
