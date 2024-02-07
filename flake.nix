5071fde0728184879bbca5a8baa446193318652b
Aos Dabbagh <25783780+aos@users.noreply.github.com>
2024-02-07 17:26:57 -0500

    switch from flake-parts to flake-utils

--- flake.nix
+++ flake.nix
@@ -3,26 +3,22 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-parts = {
-      url = "github:hercules-ci/flake-parts";
-      inputs.nixpkgs-lib.follows = "nixpkgs";
-    };
+    flake-utils.url = "github:numtide/flake-utils";
     fenix = {
       url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = inputs@{ flake-parts, nixpkgs, fenix, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" ];
-      perSystem = { pkgs, system, self', ... }:
-        let
-          cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
-          rustToolchain = fenix.packages.${system}.stable.toolchain;
-        in
-        {
-          packages.atools = (pkgs.makeRustPlatform {
+  outputs = inputs@{ self, nixpkgs, flake-utils, fenix, ... }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+        rustToolchain = fenix.packages.${system}.stable.toolchain;
+      in {
+        packages = rec {
+          atools = (pkgs.makeRustPlatform {
             cargo = rustToolchain;
             rustc = rustToolchain;
           }).buildRustPackage rec {
@@ -33,25 +29,21 @@
 
             # Hard links the binaries to the main binary
             postInstall = ''
-              ${builtins.concatStringsSep "\n"
-                (builtins.map
-                  (x: "ln $out/bin/${name} $out/bin/${pkgs.lib.removeSuffix ".rs" x}")
-                  (builtins.filter
-                    (n: n != "lib.rs" && n != "main.rs")
-                    (builtins.attrNames
-                      (builtins.readDir ./src))))}
+              ${builtins.concatStringsSep "\n" (builtins.map (x:
+                "ln $out/bin/${name} $out/bin/${pkgs.lib.removeSuffix ".rs" x}")
+                (builtins.filter (n: n != "lib.rs" && n != "main.rs")
+                  (builtins.attrNames (builtins.readDir ./src))))}
             '';
           };
-          packages.default = self'.packages.atools;
-          devShells.default = self'.packages.default.overrideAttrs (super: {
+          default = atools;
+        };
+        devShells.default = self.packages.${system}.default.overrideAttrs
+          (super: {
             nativeBuildInputs = with pkgs;
               super.nativeBuildInputs
-              ++ [
-                fenix.packages.${system}.stable.rust-analyzer
-              ];
+              ++ [ fenix.packages.${system}.stable.rust-analyzer ];
             RUST_SRC_PATH = rustToolchain;
             RUST_BACKTRACE = 1;
           });
-        };
-    };
+      });
 }
