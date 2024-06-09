7f2affbd8a8fc3b737d662dcd6f95ce2b1ae6b71
Aos Dabbagh <25783780+aos@users.noreply.github.com>
2024-06-09 15:30:34 -0400

    use const

--- src/btw.rs
+++ src/btw.rs
@@ -1,6 +1,8 @@
 use rayon::prelude::*;
 use xshell::{cmd, Shell};
 
+const BT_CONNECT_TIMEOUT: &str = "10";
+
 pub(crate) fn run(sh: &Shell) -> anyhow::Result<()> {
     let devices = cmd!(sh, "bluetoothctl devices").quiet().read()?;
     let devices = devices
@@ -18,7 +20,7 @@ pub(crate) fn run(sh: &Shell) -> anyhow::Result<()> {
 
 fn try_connect(dev: &str) -> anyhow::Result<()> {
     let sh = Shell::new()?;
-    cmd!(sh, "bluetoothctl --timeout 5 connect {dev}")
+    cmd!(sh, "bluetoothctl --timeout {BT_CONNECT_TIMEOUT} connect {dev}")
         .quiet()
         .run()
         .map_err(anyhow::Error::msg)
